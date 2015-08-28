75a83e6f93c502c514ac40cf4d320eb6af530fce
Ismael Aderdor <ikanian@ika-suse.site>
2015-08-28 15:32:01 +0200

    Updated translation to spanish v0.9.2

---
+++ es_ES/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+	'comments' => 'Comentarios',
+	'nocomments_desc' => 'Aun no hay comentarios :(',
+	'editing_comment' => 'Editar comentario',
+	'view_comment' => 'Ver comentario',
+
+	// form fields
+	'name' => 'Nombre',
+	'name_explain' => 'Nombre del autor',
+	'name_missing' => 'Por favor, introduce un nombre',
+
+	'email' => 'E-mail',
+	'email_explain' => 'E-mail del autor',
+	'email_missing' => 'Por favor, introduce una dirección de e-mail válida', // frontend message (appears on your site!)
+
+	'text' => 'Comentario',
+	'text_explain' => '',
+	'text_missing' => 'Por favor, introduce el texto del comentario', // frontend message (appears on your site!)
+
+	'status' => 'Estado',
+	'status_explain' => '',
+
+	// messages
+	'created' => '¡Tu comentario ha sido enviado!', // frontend message (appears on your site!)
+	'updated' => 'El comentario ha sido actualizado',
+	'deleted' => 'El comentario ha sido borrado',
+
+	// email notification
+	'notify_subject' => 'Se ha añadido un nuevo comentario',
+	'nofity_heading' => 'Has recibido un nuevo comentario.'
+
+);
--- es_ES/users.php
+++ es_ES/users.php
@@ -2,68 +2,66 @@
 
 return array(
 
-	'add_user' => 'A&ntilde;ade un nuevo usuario',
-	'recover_password' => 'Recupera la contrase&ntilde;a',
 	'users' => 'Usuarios',
-	'create_user' => 'Crea un nuevo usuario',
-	'reset_password' => 'Restaura la contrase&ntilde;a',
-	'reset_password_for' => 'Contrase&ntilde;a restaurada para ',
-	'reset_password_explain' => 'Por favor, introduce una contrase&ntilde;a que no olvides esta vez.',
-	'back_to' => 'Volver a ',
 
-	// english spelling confusion
-	'log_in' => 'Inicia Sesi&oacute;n', // verb
-	'login' => 'inicio de sesi&oacute;n', // noun
+	'create_user' => 'Crear nuevo usuario',
+	'add_user' => 'Añade un nuevo usuarior',
+	'editing_user' => 'Editando el perfil de %s ',
+	'remembered' => 'Conozco mi contraseña',
+	'forgotten_password' => '¿Has olvidado tu contraseña?Forgotten your password?',
 
-	'forgotten_password' => 'Olvidaste tu contrase&ntilde;a?',
-
-	'recover' => 'Recupera',
-	'editing' => 'Edici&oacute;n',
-	'profile' => 'perfil',
-
-	'real_name' => 'Nombre real',
-	'real_name_explain' => 'El nombre real del usuario. Usado en - Escrito por - (visible al p&uacute;blico).',
-	'bio' => 'Biograf&iacute;a',
-	'bio_explain' => 'Una corta biograf&iacute;a del usuario. Acepta HTML valido.',
-	'status' => 'Estado',
-	'inactive' => 'Inactivo',
-	'active' => 'Activo',
-	'status_explain' => 'El usuario no podr&aacute; iniciar sesi&oacute;n si est&aacute; inactivo.',
-	'role' => 'Rol',
+	// roles
 	'administrator' => 'Administrador',
+	'administrator_explain' => '',
+
 	'editor' => 'Editor',
+	'editor_explain' => '',
+
 	'user' => 'Usuario',
-	'role_explain' => 'El rol del usuario. Para m&aacute;s informaci&oacute;n visita la documentacion sobre <a href="//anchorcms.com/docs/roles">roles</a>.',
-	'user_details' => 'Datos del usuario',
-	'user_details_explain' => 'Crear los datos del nuevo usuario para iniciar sesi&oacute;n en Anchor.',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => 'Nombre real',
+	'real_name_explain' => '',
+
+	'bio' => 'Biografía',
+	'bio_explain' => '',
+
+	'status' => 'Estado',
+	'status_explain' => '',
+
+	'role' => 'Rol',
+	'role_explain' => '',
+
 	'username' => 'Nombre de usuario',
-	'username_explain' => 'Tu nombre de usuario deseado. Puedes cambiarlo m&aacute;s adelante.',
-	'password' => 'Contrase&ntilde;a',
-	'password_explain' => 'Y la contrase&ntilde;a correspondiente. Se puede cambiar m&aacute;s adelante.',
-	'password_blank' => 'D&eacute;jala en blanco para descartar cambios.',
+	'username_explain' => '',
+	'username_missing' => 'Por favor, introduce un nombre de usuario. Debe tener al menos %s caracteres',
+
+	'password' => 'Contraseña',
+	'password_explain' => '',
+	'password_too_short' => 'La contrasela debe contener al menos %s caracteres',
+
+	'new_password' => 'Nueva contraseña',
+
 	'email' => 'Email',
-	'email_explain' => 'El email del usuario. Necesario si el usuario pierde la contrase&ntilde;a.',
+	'email_explain' => '',
+	'email_missing' => 'Por favor, introduce una dirección de email válida',
+	'email_not_found' => 'Perfil no encontrado.',
 
-	'create' => 'Crear',
-	'update' => 'Actualizar',
-	'delete' => 'Eliminar',
-	'submit' => 'Enviar',
-	'return_users' => 'Volver a Usuarios',
+	// messages
+	'updated' => 'Perfil de usuario actualizado.',
+	'created' => 'Perfil de usuario creado.',
+	'deleted' => 'Perfil de usuario borrado.',
+	'delete_error' => 'No puedes borrar tu propio perfil',
+	'login_error' => 'Nombre de usuario o contraseña incorrectos.',
+	'logout_notice' => 'Acabas de cerrar sesión.',
+	'recovery_sent' => 'Te acabamos de enviar un email para confirmar el cambio de contraseña.',
+	'recovery_expired' => 'El identificador de recuperación de contraseña ha expirado. Por favor, inténtalo otra vez.',
+	'password_reset' => 'Tu nueva contraseña ha sido actualizada. Inicia sesión ahora.',
 
-	'username_exists' => 'El nombre de usuario ya est&aacute; en uso',
-	'missing_password' => 'Por favor, introduce una contrase&ntilde;a',
-	'invalid_email' => 'Por favor, introduce un email válido',
-	'missing_name' => 'Por favor, introduzca su nombre',
-	'missing_username' => 'Por favor, introduce un nombre de usuario',
-	'user_success_created' => 'Se ha a&ntilde;adido un nuevo usuario',
-	'user_success_updated' => 'El usuario ha sido actualizado',
-	'user_success_deleted' => 'El usuario ha sido eliminado',
-	'user_success_password' => 'Su nueva contrase&ntilde;a se ha establecido con exito',
-	'user_notice_recover' => 'Te hemos enviado un email para confirmar el cambio de contrase&ntilde;a.',
-	'user_email_recover' => 'Has solicitado restablecer la contrase&ntilde;a. Continuar en el siguiente enlace.',
-	'user_subject_recover' => 'Contrase&ntilde;a Restaurada',
-	'invalid_account' => 'Cuenta no disponible',
-	'missing_login_username' => 'Por favor, introduce tu nombre de usuario',
-	'missing_login_password' => 'Por favor, introduce tu contrase&ntilde;a'
+	// password recovery email
+	'recovery_subject' => 'Restauración de contraseña',
+	'recovery_message' => 'Acabas de pedir restaurar tu contraseña.' .
+		'Para continuar, sigue el siguiente link.' . PHP_EOL . '%s',
 
 );
